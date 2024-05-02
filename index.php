adf8176038323650ac2c25c420f7a89968dc4a54
cristlm-teclogging <167269937+cristlm-teclogging@users.noreply.github.com>
2024-05-02 00:21:39 -0600

    actualizacion del 01 / mayo en la madrugada

--- index.php
+++ index.php
@@ -36,59 +36,95 @@
                         <ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">
                             <li><a class="dropdown-item" href="http://localhost/inventario_tec/modules/antena/registrar_antena.php"> <i class="fa-solid fa-satellite-dish fa-sm"></i> Registrar Nueva Antena</a></li>
                             <li><a class="dropdown-item" href="http://localhost/inventario_tec/modules/antena/index.php"> <i class="fa-solid fa-clipboard fa-sm"></i>  Vista de Antenas</a></li>
-                            <li><a class="dropdown-item" href="http://localhost/inventario_tec/modules/kit_item/registrar_ki.php"> <i class="fa-solid fa-scroll fa-sm"></i>  Registrar kit_item</a></li>
+                            <li><a class="dropdown-item" href="http://localhost/inventario_tec/modules/kit_item/index.php"> <i class="fa-solid fa-scroll fa-sm"></i>  Registrar kit_item</a></li>
                         </ul>
                     </div>
                 </div>
 
                 <hr>
+                <?php 
+                   require "./config/conexion.php";
+
+                   // Configuración de la paginación
+                   $resultados_por_pagina = 3; // Cambia este valor según tus necesidades
+                   if (isset($_GET['pagina'])) {
+                       $pagina = $_GET['pagina'];
+                   }else{
+                       $pagina = 1;
+                   }
+
+                   $empezar_desde = ($pagina - 1) * $resultados_por_pagina;
+
+                   // Consulta SQL para obtener el total de filas
+                   $sql_total = "SELECT COUNT(*) as total FROM `kit`";
+                   $resultado_total = $conexion->query($sql_total);
+                   $total_filas = $resultado_total->fetch_assoc()['total'];
+
+                   // Calcular el número total de páginas
+                   $total_paginas = ceil($total_filas / $resultados_por_pagina);
+
+                   // Consulta SQL para obtener los datos para la página actual
+                   $sql_paginacion = "SELECT kit.num_kit as 'num_kit', kit.id_kit as 'id_kit', kit.ip as 'ip', kit.tw as 'tw', kit.status as 'status_kit', st.id_status as 'id_status', st.status as 'status' FROM kit kit LEFT JOIN status st ON kit.status = st.id_status LIMIT $empezar_desde, $resultados_por_pagina";
+                   $resultado_paginacion = $conexion->query($sql_paginacion);
+                ?>
                 <div class="col table-responsive">
-                <table class="table table-sm table-hover table-bordered ">
-                    <thead>
-                        <th>Numero_kit</th>
-                        <th>id_kit</th>
-                        <th>Ip</th>
-                        <th>Teamviewer</th>
-                        <th>Status</th>
-                        <th>Editar</th>
-                        <th>Eliminar</th>
+                    <table class="table table-sm table-hover table-bordered">
+                        <thead>
+                            <tr>
+                                <th>id_ip</th>
+                                <th>direccion ip</th>
+                                <th>num kit</th>
+                                <th>status</th>
+                                <th>Editar</th>
+                                <th>eliminar</th>                                
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php while ($row = $resultado_paginacion->fetch_assoc()):?>
+                                <tr class="">
+                                    <td><?php echo $row['num_kit'];?></td>
+                                     <td><?php echo $row['ip'];?></td>
+                                    <td><?php echo $row['tw'];?></td>
+                                    <td><?php echo $row['status'];?></td>
+                                    <td class="text-center">
+                                        <a href="./modules/editar_kit.php?num_kit=<?php echo $row['num_kit'];?>" class="btn btn-warning"> <i class="fa-solid fa-user-pen"></i></a>
 
-                    </thead>
-                    <tbody>
-                    <?php
-                       require "./config/conexion.php";
-
-                       $sql = "SELECT kit.num_kit as 'num_kit', kit.id_kit as 'id_kit', kit.ip as 'ip', kit.tw as 'tw', kit.status as 'status_kit', st.id_status as 'id_status', st.status as 'status' FROM kit kit LEFT JOIN status st ON kit.status = st.id_status";
-                   
-                       $resultado = $conexion->query($sql);
-
-                       while($row = $resultado->fetch_assoc()) { ?>
-                        <tr>
-                            <!--corregir el nombre de las propiedades-->
+                                    </td>
+                                    <td class="text-center">
+                                        <a href="./modules/eliminar_kit.php?num_kit=<?php echo $row['num_kit'];?>" class="btn btn-danger"> <i class="fas fa-trash-can"></i></a>
+                                    </td> 
+                                    <?php endwhile; ?>                                   
+                                </tr>
                             
-                            <td><?php echo $row['num_kit'];?></td>
-                            <td><?php echo $row['id_kit'];?></td>
-                            <td><?php echo $row['ip'];?></td>
-                            <td><?php echo $row['tw'];?></td>
-                            <td><?php echo $row['status'];?></td>
-                            <td class="text-center">
-                            <a href="./modules/editar_kit.php?num_kit=<?php echo $row['num_kit'];?>" class="btn btn-warning"> <i class="fa-solid fa-user-pen"></i></a>
-
-                            </td>
-                            <td class="text-center">
-                            <a href="./modules/eliminar_kit.php?num_kit=<?php echo $row['num_kit'];?>" class="btn btn-danger"> <i class="fas fa-trash-can"></i></a>
-                            </td>
-
-                        </tr>
-                        <?php  } ?>
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
                 </div>
+                    <!-- Paginación -->
+                    <nav aria-label="Page navigation example">
+    <ul class="pagination justify-content-center">
+        <?php if ($pagina > 1): ?>
+            <li class="page-item">
+                <a class="page-link" href="?pagina=<?php echo $pagina - 1; ?>">Anterior</a>
+            </li>
+        <?php endif; ?>
+        <?php for ($i = 1; $i <= $total_paginas; $i++): ?>
+            <li class="page-item <?php if ($pagina == $i) echo 'active'; ?>">
+                <a class="page-link" href="?pagina=<?php echo $i; ?>"><?php echo $i; ?></a>
+            </li>
+        <?php endfor; ?>
+        <?php if ($pagina < $total_paginas): ?>
+            <li class="page-item">
+                <a class="page-link" href="?pagina=<?php echo $pagina + 1; ?>">Siguiente</a>
+            </li>
+        <?php endif; ?>
+    </ul>
+</nav>
+
+              
 
             </div>
         </div>
     </div>
   </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </div>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
